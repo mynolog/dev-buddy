ce9439e4314f02a572c14cd084a51cff3ad69d4e
minho lee <91aiden.lee@gmail.com>
2021-07-08 00:25:24 +0900

    헤로쿠 환경 변수 설정 변경

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,9 @@ import 'dotenv/config'
 import app from './app'
 import './config/db'
 
-const PORT = process.env.PORT || 4040
+const port = process.env.PORT || 4040
 
 const handleListening = () =>
   console.log(`✅ Server listening on: http://localhost:${PORT}/api`)
 
-app.listen(PORT, handleListening)
+app.listen(port, handleListening)
