83e01715106c31f4c272d2e4f45cb7efa5f0fb94
minholeelog <91aiden.lee@gmail.com>
2021-07-13 16:18:44 +0900

    post 테이블 수정 사항 기록

--- backend/src/sql/postTable.sql
+++ backend/src/sql/postTable.sql
@@ -1,4 +1,5 @@
 ALTER TABLE `post` DROP `created_at`;
 ALTER TABLE `post` ADD `created_at` TIMESTAMP NOT NULL DEFAULT current_timestamp;
 ALTER TABLE `post` ADD `updated_at` TIMESTAMP NOT NULL DEFAULT current_timestamp ON UPDATE current_timestamp;
-
+ALTER TABLE `post` ADD `is_updated` TINYINT NOT NULL DEFAULT 0;
+ALTER TABLE `post` ADD `views` INT NOT NULL DEFAULT 0;
